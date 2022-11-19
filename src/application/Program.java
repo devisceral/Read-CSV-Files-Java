05279c1beeaed90f237d52dc7435bbe0d49c058a
André Mendes <develmendes@gmail.com>
2022-11-19 16:06:26 -0300

    version 1: simple reading with class File and Scanner

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		File file = new File("c:\\temp\\in.txt"); //your path file
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			//below will avoid null pointer exception
+			if(sc != null) {
+				sc.close();
+			}
+		}
 
 	}
 
