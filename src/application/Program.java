5b63cafeed86a10945511973d65147bccc6c1f01
André Mendes <develmendes@gmail.com>
2022-11-19 17:11:24 -0300

    Version 3: Using the 'Try with resources' block

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,19 +10,10 @@ public class Program {
 		
 		String path = "c:\\temp\\in.txt";
 		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr); //From this variable, we already have access to read the file
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
-//Read line by line
+			String line = br.readLine();
 			
-			String line = br.readLine(); //Reads a file up to the line break
-			
-//Read to end of file
-//Why different from null? Because if reached the end of the file and i call the readLine(), it returns null
 		while (line != null) {
 			System.out.println(line);
 			line = br.readLine();
@@ -31,16 +22,6 @@ public class Program {
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				br.close();
-				fr.close();				
-			}
-			catch (IOException e) {
-				e.printStackTrace(); //To screen print all stackTrace errors to help me see what happened.
-			}
-		}
-
 	}
 
 }
